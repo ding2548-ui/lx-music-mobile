8c7e3b7f9f5b61a237ac080273c0393362d45101
ding2548-ui <ding2548@gmail.com>
2026-04-27 19:32:44 +0800

    Fix: initialize car key handler in player init

--- src/core/init/player/index.ts
+++ src/core/init/player/index.ts
@@ -5,6 +5,7 @@ import initPlayerEvent from './playerEvent'
 import initWatchList from './watchList'
 import initPlayProgress from './playProgress'
 import initPreloadNextMusic from './preloadNextMusic'
+import { initCarKeyHandler } from '@/core/player/carKeyHandler'
 import initLyric from './lyric'
 
 export default async(setting: LX.AppSetting) => {
@@ -16,4 +17,5 @@ export default async(setting: LX.AppSetting) => {
   initWatchList()
   initPlayProgress()
   initPreloadNextMusic()
+  initCarKeyHandler()
 }
